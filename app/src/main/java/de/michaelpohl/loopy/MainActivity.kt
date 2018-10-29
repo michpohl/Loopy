27e87806cda5e13f5a6ce67e9277cf868eb69e10
michael <kontakt@michaelpohl.de>
2018-10-29 19:07:00 +0100

    Add logging tools

--- app/src/main/java/de/michaelpohl/loopy/MainActivity.kt
+++ app/src/main/java/de/michaelpohl/loopy/MainActivity.kt
@@ -1,13 +1,20 @@
 package de.michaelpohl.loopy
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import de.michaelpohl.loopy.ui.main.MainFragment
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+            Timber.d("it works")
+        }
+
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
