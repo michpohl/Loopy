c919138eab5e79b215ed33c67d5857e8b646ac9d
michael <kontakt@michaelpohl.de>
2018-10-29 18:45:40 +0100

    Add new empty Kotlin project

---
+++ app/src/main/java/de/michaelpohl/loopy/MainActivity.kt
@@ -0,0 +1,19 @@
+package de.michaelpohl.loopy
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import de.michaelpohl.loopy.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/de/michaelpohl/loopy/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package de.michaelpohl.loopy.ui.main
+
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
