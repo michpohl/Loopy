7e5316828182e074563f59bc60730cda8e07306f
Michael Pohl <contact@michaelpohl.de>
2020-08-24 07:10:24 +0200

    Turn resampling off for now - failure :-(

--- app/src/main/cpp/StorageDataSource.cpp
+++ app/src/main/cpp/StorageDataSource.cpp
@@ -75,66 +75,66 @@ StorageDataSource *StorageDataSource::newFromStorageAsset(AMediaExtractor &extra
                                      targetProperties);
     } else {
         LOGD("SampleRate is off");
-
-
-        float *inputBuffer;
-        LOGD("Casting");
-        inputBuffer = reinterpret_cast<float *>(decodedData);
-        LOGD("Size of inputBuffer: %zu\n", sizeof(inputBuffer));
-        LOGD("Divide by this to get the number of samples: %i", sizeof(int16_t));
-        auto numSamples2 = bytesDecoded / sizeof(int16_t);
-
 //
-        float *outputBuffer2;    // multi-channel buffer to be filled
-        auto outputBuffer3 = std::make_unique<float[]>(numSamples);
-        int numInputFrames;  // number of frames of input
 //
-//        // TODO is this correct?
-//        numInputFrames = numSamples2/16;
-        numInputFrames = 192;
-        LOGD("numInputFrames: %i", numInputFrames);
-        LOGD("numSamples: %i", numSamples);
-
-
-        int numOutputFrames = 0;
-        int channelCount = 2;    // 1 for mono, 2 for stereo // TODO needs to be collected from file too
-
-        resampler::MultiChannelResampler *mResampler = resampler::MultiChannelResampler::make(
-                2, // channel count
-                44100, // input sampleRate
-                48000, // output sampleRate
-                resampler::MultiChannelResampler::Quality::Best); // conversion quality
-
-        int inputFramesLeft = numInputFrames;
+//        float *inputBuffer;
+//        LOGD("Casting");
+//        inputBuffer = reinterpret_cast<float *>(decodedData);
+//        LOGD("Size of inputBuffer: %zu\n", sizeof(inputBuffer));
+//        LOGD("Divide by this to get the number of samples: %i", sizeof(int16_t));
+//        auto numSamples2 = bytesDecoded / sizeof(int16_t);
 //
-        while (inputFramesLeft > 0) {
-            LOGD("frames left: %i", inputFramesLeft);
-//            LOGD("while...%i", inputFramesLeft);
-            if (mResampler->isWriteNeeded()) {
-//                LOGD("Write is needed");
-                mResampler->writeNextFrame(inputBuffer);
-                inputBuffer += channelCount;
-                inputFramesLeft--;
-            } else {
-//                LOGD("Write is not needed");
-                mResampler->readNextFrame(outputBuffer2);
-
-                outputBuffer2 += channelCount;
-                numOutputFrames++;
-            }
-        }
-
-        for (int i = 0; i < numSamples; i++) {
-            outputBuffer3.get()[i] = outputBuffer2[i];
-        }
-
-        return new StorageDataSource(std::move(outputBuffer3),
-                                     numSamples,
-                                     targetProperties);
-        delete mResampler;
+////
+//        float *outputBuffer2;    // multi-channel buffer to be filled
+//        int numInputFrames;  // number of frames of input
+////
+////        // TODO is this correct?
+////        numInputFrames = numSamples2/16;
+//        numInputFrames = 192;
+//        LOGD("numInputFrames: %i", numInputFrames);
+//        LOGD("numSamples: %i", numSamples);
+//        auto outputBuffer3 = std::make_unique<float[]>(numSamples2);
+//
+//
+//        int numOutputFrames = 0;
+//        int channelCount = 2;    // 1 for mono, 2 for stereo // TODO needs to be collected from file too
+//
+//        resampler::MultiChannelResampler *mResampler = resampler::MultiChannelResampler::make(
+//                2, // channel count
+//                44100, // input sampleRate
+//                48000, // output sampleRate
+//                resampler::MultiChannelResampler::Quality::Best); // conversion quality
+//
+//        int inputFramesLeft = numInputFrames;
+////
+//        while (inputFramesLeft > 0) {
+//            LOGD("frames left: %i", inputFramesLeft);
+////            LOGD("while...%i", inputFramesLeft);
+//            if (mResampler->isWriteNeeded()) {
+////                LOGD("Write is needed");
+//                mResampler->writeNextFrame(inputBuffer);
+//                inputBuffer += channelCount;
+//                inputFramesLeft--;
+//            } else {
+////                LOGD("Write is not needed");
+//                mResampler->readNextFrame(outputBuffer2);
+//
+//                outputBuffer2 += channelCount;
+//                numOutputFrames++;
+//            }
+//        }
+//        LOGD("While loop ended");
+//        delete mResampler;
+//        for (int i = 0; i < numSamples2; i++) {
+//            outputBuffer3.get()[i] = outputBuffer2[i];
+//        }
+//
+//        return new StorageDataSource(std::move(outputBuffer3),
+//                                     numSamples2,
+//                                     targetProperties);
     }
     LOGD("Before return");
-    return new StorageDataSource(std::move(outputBuffer),
-                                 numSamples,
-                                 targetProperties);
+//    return new StorageDataSource(std::move(outputBuffer),
+//                                 numSamples,
+//                                 targetProperties);
 }
