cd7aa15b1acfa9c9c64c8e874932e0ee86dd5169
Michael Pohl <contact@michaelpohl.de>
2020-09-01 10:34:09 +0200

    Add sorting to player

---
+++ app/src/main/java/de/michaelpohl/loopy/ui/main/player/adapter/PlayerItemSorting.kt
@@ -0,0 +1,12 @@
+package de.michaelpohl.loopy.ui.main.player.adapter
+
+import com.example.adapter.adapter.Sorting
+import de.michaelpohl.loopy.common.AudioModel
+import de.michaelpohl.loopy.common.FileModel
+
+class PlayerItemSorting : Sorting<AudioModel>() {
+    override fun sort(input: List<AudioModel>): List<AudioModel> {
+        return input.distinct()
+    }
+
+}
